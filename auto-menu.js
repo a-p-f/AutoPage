278212680664054db9f1f8f62222bd88049d407f
Alex Fischer <alex_fischer_81@hotmail.com>
2020-05-22 17:13:02 -0600

    Cleaned up auto menu

--- auto-menu.js
+++ auto-menu.js
@@ -2,44 +2,69 @@
 'use strict';
 
 var menu = document.querySelector('.AutoMenu'); if (!menu) return;
-// var openButton = (function() {
-// 	var b = document.createElement('button');
-// 	b.classList.add('AutoMenuOpenButton');
-// 	b.innerText = '☰';
-// 	b.onclick = function() {toggleMenu(true)}
-// 	menu.parentElement.insertBefore(b, menu);
-// 	return b;
-// })();
-// var closeButton = (function() {
+// Create open/close buttons
+var openButton = (function() {
+	var b = document.createElement('button');
+	b.classList.add('AutoMenuOpenButton');
+	b.innerText = '☰';
+	b.onclick = activateMenu;
+	menu.parentElement.insertBefore(b, menu);
+	return b;
+})();
+var closeButton = (function() {
+	var b = document.createElement('button');
+	b.classList.add('AutoMenuCloseButton');
+	b.innerText = '✕';
+	b.onclick = deactivateMenu;
+	menu.insertBefore(b, menu.firstElementChild);
+	return b;
+})();
 
-// })();
-
-// function toggleOpenButton(show) {
-
-// }
-// function toggleMenu(active) {
-
-// }
-
-// Add menu button, add click handler
-var menuButton = document.createElement('button');
-menuButton.classList.add('AutoMenuButton');
-menuButton.innerText = '☰';
-menuButton.onclick = function() {
-	menuButton.classList.toggle('AutoMenuButton-menuShown');
-	menu.classList.toggle('AutoMenu-shown');
-}
-menu.parentElement.insertBefore(menuButton, menu);
-
-// Hide the menu button when the the menu is on-screen
+// Hide the open button when the the menu is on-screen
 new IntersectionObserver(function(entries) {
 	if (entries[0].isIntersecting) {
-		menuButton.classList.remove()
-		menuButton.style.display = 'none';
+		openButton.classList.remove('AutoMenuOpenButton-visible')
+		setTimeout(function() {
+			openButton.style.display = 'none';
+		}, 300);
 	}
 	else {
-		menuButton.style.display = 'block';
+		openButton.style.display = 'block';
+		requestAnimationFrame(function() {requestAnimationFrame(function() {
+			openButton.classList.add('AutoMenuOpenButton-visible')
+		})});
 	}
 }, {}).observe(menu);
+
+var overflowX, overflowY;
+function lockScroll() {
+	/*
+		Note - we don't worry about reflows due scroll bars showing/hiding,
+		because this will generally only be used on mobile devices, where scrollbars never take up width
+	*/
+	var bs = document.body.style;
+	overflowX = bs.overflowX;
+	overflowY = bs.overflowY;
+	bs.overflowX = 'hidden';
+	bs.overflowY = 'hidden';
+}
+function releaseScroll() {
+	var bs = document.body.style;
+	bs.overflowX = overflowX;
+	bs.overflowY = overflowY;
+}
+
+function activateMenu() {
+	menu.classList.add('AutoMenu-active');
+	openButton.style.display = 'none';
+	closeButton.style.visibility = 'visible';
+	lockScroll();
+}
+function deactivateMenu() {
+	menu.classList.remove('AutoMenu-active');
+	openButton.style.display = 'block';
+	closeButton.style.visibility = 'hidden';
+	releaseScroll();
+}
 
 })();
