fb81490da34b1d856b7c2ba6d7fbb1c35c3bd73d
Alex Fischer <alex_fischer_81@hotmail.com>
2020-05-28 17:21:43 -0600

    top menu layout working

--- auto-menu.js
+++ auto-menu.js
@@ -10,43 +10,9 @@
 (function() {
 'use strict';
 
-var menu = document.querySelector('.AutoMenu'); if (!menu) return;
-var isOpen = false;
-// Create open/close buttons
-var openButton = (function() {
-	var b = document.createElement('button');
-	b.classList.add('AutoMenuOpenButton');
-	b.innerText = '☰';
-	b.onclick = activateMenu;
-	menu.parentElement.insertBefore(b, menu);
-	return b;
-})();
-var closeButton = (function() {
-	var b = document.createElement('button');
-	b.classList.add('AutoMenuCloseButton');
-	b.innerText = '✕';
-	b.onclick = deactivateMenu;
-	menu.insertBefore(b, menu.firstElementChild);
-	return b;
-})();
-
-// Hide the open button when the the menu is on-screen
-new IntersectionObserver(function(entries) {
-	if (entries[0].isIntersecting) {
-		// TODO - remove display none callback, set tabIndex = -1, aria-hidden, and pointer-events none?
-		// Would decouple the timeout from styles a little bit
-		openButton.classList.remove('AutoMenuOpenButton-visible');
-		setTimeout(function() {
-			openButton.style.display = 'none';
-		}, 300);
-	}
-	else {
-		openButton.style.display = 'block';
-		requestAnimationFrame(function() {requestAnimationFrame(function() {
-			openButton.classList.add('AutoMenuOpenButton-visible')
-		})});
-	}
-}, {}).observe(menu);
+function gID(id) {
+	return document.getElementById(id);
+}
 
 var overflowX, overflowY;
 function lockScroll() {
@@ -66,31 +32,66 @@ function releaseScroll() {
 	bs.overflowY = overflowY;
 }
 
-function activateMenu() {
-	if (isOpen) return
-	isOpen = true;
-	menu.classList.add('AutoMenu-active');
-	openButton.style.display = 'none';
-	closeButton.style.visibility = 'visible';
-	lockScroll();
-}
-function deactivateMenu() {
-	if (!isOpen) return
-	isOpen = false;
-	menu.classList.remove('AutoMenu-active');
-	openButton.style.display = 'block';
-	closeButton.style.visibility = 'hidden';
-	releaseScroll();
-}
+// Prepare the AutoMenu
+(function() {
+	var menu = gID('AutoMenu'); if (!menu) return;
+	var openButton = gID('AutoMenuOpenButton');
+	var closeButton = gID('AutoMenuCloseButton');
+	var isActive = false;
 
-addEventListener('scroll', function(e) {
-	/*
-		User can't scroll the document while the menu is open.
-		If the document scrolls, it must be because the user clicked a hash-link inside the menu. 
-	*/
-	if (e.target == document) {
-		deactivateMenu();
+	openButton.onclick = activateMenu;
+	closeButton.onclick = function() {
+		if (isActive) {
+			deactivateMenu();
+		}
+		else {
+			menu.nextElementSibling.scrollIntoView();
+		}
 	}
-});
 
+	// // Create open/close buttons
+	// var openButton = (function() {
+	// 	var b = document.createElement('button');
+	// 	b.classList.add('AutoMenuOpenButton');
+	// 	b.innerText = '☰';
+	// 	b.onclick = activateMenu;
+	// 	menu.parentElement.insertBefore(b, menu);
+	// 	return b;
+	// })();
+	// var closeButton = (function() {
+	// 	var b = document.createElement('button');
+	// 	b.classList.add('AutoMenuCloseButton');
+	// 	b.innerText = '✕';
+	// 	b.onclick = deactivateMenu;
+	// 	menu.insertBefore(b, menu.firstElementChild);
+	// 	return b;
+	// })();
+
+
+
+	function activateMenu() {
+		if (isActive) return
+		isActive = true;
+		menu.classList.add('active');
+		lockScroll();
+	}
+	function deactivateMenu() {
+		if (!isActive) return
+		isActive = false;
+		menu.classList.remove('active');
+		releaseScroll();
+	}
+
+	addEventListener('scroll', function(e) {
+		/*
+			User can't scroll the document while the menu is open.
+			If the document scrolls, it must be because the user clicked a hash-link inside the menu. 
+		*/
+		if (e.target == document) {
+			deactivateMenu();
+		}
+	});
+})();
+
+document.documentElement.classList.remove('auto-menu-loading');
 })();
